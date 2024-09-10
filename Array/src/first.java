c80c8e16670e45460ba5023c4737f59d0f3829ef
gourabofficial <gangulygourab3@gmail.com>
2024-09-10 23:22:03 +0530

    start a basic example

---
+++ Array/src/first.java
@@ -0,0 +1,13 @@
+public class first {
+    public static void main(String[] args) {
+        int[] arr = new int[6];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 112;
+        arr[3] = 11;
+
+        for(int i: arr){
+            System.out.println(i);
+        }
+    }
+}
