20730daa8e080acee6f02096286585a0a29e53e5
gourabofficial <gangulygourab3@gmail.com>
2024-09-11 20:39:56 +0530

    array list in java

---
+++ Array/arraylist.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+
+
+
+public class arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(127);
+        list.add(19992);
+        list.add(126);
+        list.add(1432);
+        list.add(142);
+        System.out.println(list);
+
+        list.remove(0);
+        list.add(0, 99);
+        list.clear();
+        //after removing the first element and adding 99 at the first index
+        System.out.println(list);
+    }
+}
