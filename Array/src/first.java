7a110921a4716a0bfe3d3e87a48615c33873146e
gourabofficial <gangulygourab3@gmail.com>
2024-09-12 09:55:42 +0530

    updated

--- Array/src/first.java
+++ Array/src/first.java
@@ -1,16 +1,15 @@
-import java.util.Scanner;
 public class first {
     public static void main(String[] args) {
-        // int[] arr = new int[6];
-        // arr[0] = 1;
-        // arr[1] = 2;
-        // arr[2] = 112;
-        // arr[3] = 11;
+        int[] arr = new int[6];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 112;
+        arr[3] = 11;
 
-        // for(int i: arr){
-        //     System.out.print(i + " ");
-        // }
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        for(int i: arr){
+            System.out.print(i + " ");
+        }
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
     }
 }
--- Array/src/second.java
+++ Array/src/second.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class second {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
         System.out.print("Enter your array size: ");
-        int arrsize = sc.nextInt();
+        int arrsize = in.nextInt();
         int[]arr = new int[arrsize];
         System.out.print("your arr: ");
         for (int i = 0; i < arrsize; i++) {
-        arr[i] = sc.nextInt();
+        arr[i] = in.nextInt();
         }
 
         System.out.print("Array element are: ");
