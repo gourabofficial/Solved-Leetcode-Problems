e7bb9095105b8143afae13fe7a5778aefa60f261
gourabofficial <gangulygourab3@gmail.com>
2024-09-16 20:04:51 +0530

    Refactor linearSearch to include user input and display the target element index

--- Searching/linearSearch.java
+++ Searching/linearSearch.java
@@ -1,14 +1,33 @@
-class linearSearch{
+
+import java.util.Scanner;
+
+class linearSearch {
+
     public static void main(String[] args) {
-        int[]arr = {1,2,3,48,9,0,10};
-        int result = searching(arr, 10);
-        System.out.println("the target element is: " + result);
+        Scanner input = new Scanner(System.in);
+        int[] arr = {1, 2, 3, 48, 9, 0, 10};
+
+        System.out.print("Enter the element you search in this array: ");
+        int element = input.nextInt();
+
+        int result = searching(arr, element);
+
+        if (result == -1) {
+            System.out.println("Element not found int he array");
+        } else {
+            System.out.println("the element is : " + element);
+        }
+
+        System.out.println("the target element index is: " + result);
     }
-    public static int searching(int[]arr,int target){
+
+    public static int searching(int[] arr, int target) {
         for (int i = 0; i < arr.length; i++) {
-            if( arr[i] == target)
-            return arr[i];
+            if (arr[i] == target) {
+                return i;
+            }
         }
         return -1;
-    } 
+    }
+
 }
