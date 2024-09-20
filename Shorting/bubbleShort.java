6a8690058c9dd609b5af45ad96b8ae906b674e0c
gourabofficial <gangulygourab3@gmail.com>
2024-09-20 22:07:25 +0530

    bubble short implement

--- Shorting/bubbleShort.java
+++ Shorting/bubbleShort.java
@@ -4,20 +4,24 @@ class bubbleShort {
     public static void main(String[] args) {
         int[] arr = {20, 13, 10, 4, 2, 46, 8};
         int size = arr.length;
-        int temp = 0;
+        int temp;
 
         System.out.print("Before shorting: ");
         for (int num : arr) {
             System.out.print(num + " ");
         }
 
-        for (int i = 0; i <= size; i++) {
-            for (int j = 0; j < size - 1; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
+            }
+            System.out.println(" ");
+            for (int num : arr) {
+                System.out.print(num + " ");
             }
         }
         System.out.println(" ");
