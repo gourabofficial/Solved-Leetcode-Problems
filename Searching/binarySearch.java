e13a6ea16aa68251bcc69fbd511c9dd241ca97e3
gourabofficial <gangulygourab3@gmail.com>
2024-09-20 11:13:35 +0530

    binary search algo

--- Searching/binarySearch.java
+++ Searching/binarySearch.java
@@ -2,34 +2,30 @@
 class binarySearch {
 
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5,  6, 57, };
-        int target = 6;
-        int result = binarySearching(arr, target);
-        System.out.println("the target element index is: " + result);
-       
-    }
+        int[] arr = {3, 5, 7, 9, 11, 13, 15, 17};
+        int target_item = 13;
+        int result = binarySearching(arr, target_item);
 
-    public static int binarySearching(int arr[], int target) {
-
-        int start = 0;
-        int end = arr.length - 1;
-        int mid;
-
-        while (start <= end) {
-            mid = (start + end) / 2;
-            if (arr[mid] == target) {
-                return mid;
-            } else {
-                if(target < arr[mid]){
-                    end = mid - 1;
-                }else{
-                    start = mid + 1;
-                }
-             }
-
-            }
-            
-            return -1;
+        if (result != -1) {
+            System.out.println("element in index of : " + result);
+        } else {
+            System.out.println("element not found..");
         }
     }
 
+    public static int binarySearching(int[] a, int target) {
+        int left = 0;
+        int right = a.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (a[mid] == target) {
+                return mid;
+            } else if (a[mid] < target) {
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
