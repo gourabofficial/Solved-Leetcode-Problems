03b7cedbceec15102293e192b575720fe2f408ee
gourabofficial <gangulygourab3@gmail.com>
2024-09-21 10:51:58 +0530

    insertionShort implement

--- Shorting/insertionShort.java
+++ Shorting/insertionShort.java
@@ -1,31 +1,34 @@
+
 public class insertionShort {
+
     public static void main(String[] args) {
         int[] arr = {20, 13, 10, 4, 2, 46, 8};
         int size = arr.length;
+        int key;
 
         System.out.print("Before sorting: ");
         for (int num : arr) {
             System.out.print(num + " ");
         }
 
-        // Insertion Sort Algorithm
         for (int i = 1; i < size; i++) {
-            int key = arr[i]; // The element to be inserted
+            key = arr[i];
             int j = i - 1;
-
-            // Shift elements of arr[0..i-1], that are greater than key, to one position ahead
             while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-            }
-            arr[j + 1] = key; // Insert the key at the correct position
-        }
+                //shipting
 
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+
+        }
         System.out.println();
         System.out.print("After sorting: ");
         for (int num : arr) {
             System.out.print(num + " ");
         }
+
     }
-    
+
 }
